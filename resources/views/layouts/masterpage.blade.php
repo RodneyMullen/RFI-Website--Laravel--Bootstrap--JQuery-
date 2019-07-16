f6d82b779012746887254531594a55c7bdea3874
Rodney <Rodney@Rising>
2019-07-16 10:45:11 +0100

    v2 -  Login Page to Webadministration

--- resources/views/layouts/masterpage.blade.php
+++ resources/views/layouts/masterpage.blade.php
@@ -5,78 +5,96 @@
  */
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+ <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
 <title>@yield('title', 'Home')</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link href="{{ asset('css/app-rfi.css') }}" rel="stylesheet">
+<link href="{{ asset('css/rfi.css') }}" rel="stylesheet">
+  
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<style type="text/css">
-    body{
-    	padding-top: 70px;
-		
-    }
-	.navbar-default
-	{
-	    background-color: #006C28;
-	}
-	
-	.navbar-nav
-	{
-	   padding-left: 20px;
-	}
-	
-	.navbar-default .navbar-nav > li > a {
- 
-		color: #FFFFFF;
- 	}
- 
-	.navbar-default .navbar-nav > li > a:hover, .navbar-default .navbar-nav > li > a:focus {
- 
-		color: #e5dbdb;
- 
-	}
-	
-	.list-group a:focus, .list-group a:hover
-	{
-	  
-	  	 background-color: #006C28;
-	  color: #FFFFFF;
-	}
-	
-	.sidebar-nav
-	{
-		padding-top: 20px;
-		
-	}	
-	
-	
+<script src="{{ asset('js/app.js') }}" defer></script>
 
-</style>
 </head>
 <body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                   <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}" id="registerlink">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                         <li class="nav-item">
+                                <a class="nav-link" id="webpageadminlink" href="{{route('webpageadmin')}}"> Administration </a>
+                            </li>   
+                        <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} 
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                               
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12">
-		<img src="storage/img/HeaderImage.png">
-	</div>
-</div>
+                    <img src="storage/img/HeaderImage.png" class="img-fluid" alt="Responsive image">
+                </div>
+
+        </div> 
     
 @include('header')
+   
+        
+</div>
 <div class="container">
-	@section('JumboTron')
+	@section('content')
         <div class="jumbotron">
 		<h1>Opps!! Something happened</h1>
                 <p>The page you are searching for does not exist.  Please try again</p>
                 
         </div>
         @show
-        <div class =" container"> 
-            @yield(' content')
-        </div>
+        
 
   
         
